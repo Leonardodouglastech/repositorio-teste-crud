fe8f560f0132a544fc4092ee955347f0345b52d0
Tech com Káthia <125410101+techcomkathia@users.noreply.github.com>
2024-05-04 19:01:37 -0300

    Atual

--- telaCrud.py
+++ telaCrud.py
@@ -2,20 +2,19 @@ import streamlit as st
 
 from funcoesCrud import cadastrar
 
-
-
+#cadastro
 st.title('Sistema Dieguinho Alimentos - ME')
-st.markdown('## Cadastro de Produtos')
 
-nome = st.text_input('Nome do produto', placeholder='Nome do produto com no máximo 50 caracteres')
+col1, col2 = st.columns(2)
+containerCadastrar = st.container(border=True)
+containerCadastrar.markdown('## Cadastro de Produtos ##')
 
-imagem = st.text_input('Imagem do produto', placeholder='Url da imagem do produto com até 100 caracteres')
-
-codigo = st.text_input('Código do produto', placeholder='Código do produto')
-
-preco = float(st.number_input('Preço produto'))
-
-btnCadastrarProduto = st.button('Cadastrar Produto')
+with containerCadastrar:
+    nome = st.text_input('Nome do produto', placeholder='Nome do produto com no máximo 50 caracteres')
+    imagem = st.text_input('Imagem do produto', placeholder='Url da imagem do produto com até 100 caracteres')
+    codigo = st.text_input('Código do produto', placeholder='Código do produto')
+    preco = float(st.number_input('Preço produto'))
+    btnCadastrarProduto = st.button('Cadastrar Produto')
 
 if btnCadastrarProduto:
     cadastrar(nome, preco, codigo, imagem)
